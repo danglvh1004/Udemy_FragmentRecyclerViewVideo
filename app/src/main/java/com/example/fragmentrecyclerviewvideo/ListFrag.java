029d944cda8ae1dfbd9f2969618c572a605a2502
danglvh1004 <lvhd.hdlv@gmail.com>
2021-01-25 14:30:14 +0700

    Commit

---
+++ app/src/main/java/com/example/fragmentrecyclerviewvideo/ListFrag.java
@@ -0,0 +1,30 @@
+package com.example.fragmentrecyclerviewvideo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ListFrag#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ListFrag extends Fragment {
+
+    private String mParam2;
+
+    public ListFrag() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+}
